171707052dfef8bff34fcdf17964d7757f336c89
Syed Jafri <syed.jafri2@ucalgary.ca>
2018-09-16 11:17:28 -0600

    Start round

--- include/eosbulls/eosbulls.hpp
+++ include/eosbulls/eosbulls.hpp
@@ -2,20 +2,55 @@
 
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/time.hpp>
+#include <eosiolib/multi_index.hpp>
+
 #include <string>
 
 namespace eosio {
     using std::string;
+    using boost::container::flat_map;
+    using eosio::time_point;
 
     class eosbulls: public contract {
         public:
             eosbulls( account_name self ):contract(self){}
 
-            void ontransfer( const account_name from,
-                             const account_name to,
-                             const asset& quantity,
-                             const std::string& memo );
+            void ontransfer( const name     from,
+                             const name     to,
+                             const asset&   quantity,
+                             const string&  memo );
 
+            void startround( const uint64_t& round_number,
+                             const asset&    total_pot,      // Carried over from last round
+                             const uint64_t& inflation_rate,
+                             const uint64_t& last_price );   // Price to start at (usually $5)
+
+            void   endround( const uint64_t round_number ); // End round
+            void     payout( const uint64_t round_number ); // Pay out Winner + Pot (after round ends)
+            void clearround( const uint64_t round_number ); // Free up RAM (after round ends)
+
+    private:
+            struct round {
+                uint64_t       round_number;
+                asset          total_pot;
+                asset          total_referral_payout;
+
+                flat_map<symbol_type, uint64_t>                  total_keys_used;
+                flat_map<symbol_type, uint64_t>                  total_keys_bought;
+                flat_map<symbol_type, flat_map<name, uint64_t>>  player_keys_used;
+
+                account_name   last_key_owner;
+                symbol_type    last_key_team;
+                time_point     last_key_use_time;
+
+                uint64_t       inflation_rate;
+                uint64_t       last_price;
+
+                uint64_t primary_key()const { return round_number; }
+            };
+
+            typedef eosio::multi_index<N(rounds), round> rounds;
     };
 }
 
--- src/eosbulls.cpp
+++ src/eosbulls.cpp
@@ -1,12 +1,48 @@
 #include <eosbulls/eosbulls.hpp>
 
+constexpr account_name bulls_token_contract = N(eosbullscoin);
+constexpr account_name eos_token_contract = N(eosio.token);
+constexpr account_name maria_contract = N(mariaeyeroll);
+
 namespace eosio {
-    void eosbulls::ontransfer( const account_name from,
-                           const account_name to,
-                           const asset& quantity,
-                           const std::string& memo )
+    static time_point current_time_point() {
+        const static time_point ct{ microseconds{ static_cast<int64_t>( current_time() ) } };
+        return ct;
+    }
+
+    void eosbulls::startround( const uint64_t& round_number,
+                               const asset&    total_pot,      // Carried over from last round
+                               const uint64_t& inflation_rate,
+                               const uint64_t& last_price )    // Price to start at (usually $5)
     {
-        eosio::print(from, " ", to, " ", quantity, " ", memo);
+        rounds rounds_table(_self, _self);
+
+        rounds_table.emplace(_self, [&]( auto r ) {
+            r.round_number = round_number;
+            r.total_pot = total_pot;
+
+            r.total_referral_payout.amount = 0;
+            r.total_referral_payout.symbol = S(4, EOS);
+
+            r.total_keys_used = flat_map<symbol_type, uint64_t> ();
+            r.total_keys_bought = flat_map<symbol_type, uint64_t> ();
+            r.player_keys_used = flat_map<symbol_type, flat_map<name, uint64_t>> ();
+
+            r.last_key_owner = maria_contract;
+            r.last_key_team = S(0, BULLS);
+            r.last_key_use_time = current_time_point();
+
+            r.inflation_rate = inflation_rate;
+            r.last_price = last_price;
+        });
+    }
+
+    void eosbulls::ontransfer( const name from,
+                               const name to,
+                               const asset&       quantity,
+                               const std::string& memo )
+    {
+        // eosio::print(from, " ", to, " ", quantity, " ", memo);
     }
 
     extern "C" {
@@ -14,9 +50,16 @@ namespace eosio {
             auto self = receiver;
 
             // If transfer from system contract eosio.token
-            if (code == N(eosio.token) && action == N(transfer)) {
+            if (code == eos_token_contract && action == N(transfer)) {
                 eosbulls contract(self);
                 eosio::execute_action(&contract, &eosbulls::ontransfer);
+            }
+
+            // Start round
+            if (code == maria_contract && action == N(startround)) {
+
+                eosbulls contract(self);
+                eosio::execute_action(&contract, &eosbulls::startround);
             }
         }
     } // ABI forwarder
